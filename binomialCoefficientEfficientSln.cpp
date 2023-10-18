317950bbbaecc3e1821c4c1cf80c93efd76efcd6
Hridoy <118050603+hridoyPRGMR@users.noreply.github.com>
2023-10-18 19:40:02 +0600

    Add files via upload

---
+++ binomialCoefficientEfficientSln.cpp
@@ -0,0 +1,33 @@
+// A naive recursive C++ implementation
+#include <bits/stdc++.h>
+using namespace std;
+
+// Returns value of Binomial Coefficient C(n, k)
+int binomialCoeff(int n, int k)
+{
+	// Base Cases
+	if (k > n)
+		return 0;
+	if (k == 0 || k == n)
+        return 1;
+
+    int res=binomialCoeff(n - 1, k - 1)
+		+ binomialCoeff(n - 1, k);
+    cout<<res<<endl;
+
+	// Recur
+	return binomialCoeff(n - 1, k - 1)
+		+ binomialCoeff(n - 1, k);
+}
+
+/* Driver code*/
+int main()
+{
+	int n = 5, k = 2;
+	cout << "Value of C(" << n << ", " << k << ") is "
+		<< binomialCoeff(n, k);
+	return 0;
+}
+
+// This is code is contributed by rathbhupendra
+
