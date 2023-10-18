115341beaa0a38304c52130be8d543a604cdc670
Hridoy <118050603+hridoyPRGMR@users.noreply.github.com>
2023-10-18 19:39:15 +0600

    Add files via upload

---
+++ primeNumber.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool prime(int n){
+
+    if(n==2)return true;
+    if(n<2 || n%2==0 )return false;
+
+    for(int i=3; i<sqrt(n);i+=2){
+        if(n%i==0)return false;
+    }
+    return true;
+}
+
+int main(){
+
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        cout<<prime(n)<<endl;
+    }
+
+return 0;
+}
+
