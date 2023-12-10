f2f0e8bd571219b4af51e7d1424fb519e307fdc5
Hridoy <118050603+hridoyPRGMR@users.noreply.github.com>
2023-12-10 17:51:54 +0600

    Create squareRoot.java

---
+++ squareRoot.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class squareRoot{
+
+	static double sqrt(int n,int p){
+		double root=n;
+		double increment=1.0/Math.pow(10,p);
+    //fastest
+		while(Math.abs(root*root-n)>increment){
+			root=(root+n/root)/2.0;
+			System.out.println(root);
+		}
+		return root;
+
+		/*
+    //slowed
+		int integerPart=0;
+		while(integerPart*integerPart<=n){
+			integerPart++;
+		}
+		integerPart--;
+
+		root=0.0;
+
+		for(int i=0; i<p; i++){
+			while(root*root<=n){
+				root+=increment;
+				System.out.println(i+" "+root);
+			}
+			root-=increment;
+			increment/=10;
+		}
+		return root;
+		*/
+	}
+
+	public static void main(String []args){
+		int n=40;
+		int p=4;
+
+		System.out.println(sqrt(n,p));
+	}
+}
